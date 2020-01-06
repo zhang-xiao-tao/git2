332ab6b953dc405651da25b5a64dd16fd930a204
zhang-xiao-tao <zhang_tao_o422>
2020-01-06 18:40:22 +0800

    User实体类

--- git-test1/src/main/java/com/youzhong/entity/User.java
+++ git-test1/src/main/java/com/youzhong/entity/User.java
@@ -2,5 +2,13 @@ package com.youzhong.entity;
 
 public class User {
 
+    private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
